049d05d3cc0fce2e721b4dd21013c5436723e1ff
Gastondanielx4 <tochircentral@gmail.com>
2022-05-30 16:12:08 -0300

    05up

--- React/small-form-test/src/App.js
+++ React/small-form-test/src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
           <h1>Small Form Test</h1>
-          <SelectSmall />
       </header>
+      <SelectSmall />
     </div>
   );
 }
--- React/small-form-test/src/componentes/Selector.js
+++ React/small-form-test/src/componentes/Selector.js
@@ -1,50 +1,61 @@
-import * as React from 'react';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+    import * as React from 'react';
+    import InputLabel from '@mui/material/InputLabel';
+    import MenuItem from '@mui/material/MenuItem';
+    import FormControl from '@mui/material/FormControl';
+    import Select from '@mui/material/Select';
+    
+    const dataBase = [
+        {id:1,
+        name: "---------------",
+        rol: "----------------"},
+        {id:2,
+        name: "Matias Heredia",
+        rol: "Erudito"},
+        {id:3,
+        name: "Benjamin Quattrocchi",
+        rol: "Aprendiz"},
+        {id:4,
+        name: "Agustina Heredia",
+        rol: "Fenomeno"}
+    ]
+    export default function SelectLabels() {
+        const [db, setDb] = React.useState({dataBase});
+         const [db1, setDb1] = React.useState({dataBase});
 
-const dataBase = [
-    {id:1,
-    name: "---------------",
-    rol: "----------------"},
-    {id:2,
-    name: "Matias Heredia",
-    rol: "Erudito"},
-    {id:3,
-    name: "Benjamin Quattrocchi",
-    rol: "Aprendiz"},
-    {id:4,
-    name: "Agustina Heredia",
-    rol: "Fenomeno"}
-]
-
-export default function SelectSmall() {
-  const [age, setAge] = React.useState('');
-
-  const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value);
-  };
-
-  return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="demo-select-small">Name</InputLabel>
-      <Select
-        labelId="demo-select-small"
-        id="demo-select-small"
-        value={age}
-        label="Age"
-        onChange={handleChange}
-      >
-          {dataBase.forEach(element => {
-          });}
-        {/* <MenuItem value="">
-          <em>None</em>
-        </MenuItem>
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem> */}
-      </Select>
-    </FormControl>
+        const handleChange = (e) => {
+            setDb(e.target.value);
+            setDb1(e.target.value)
+        };
+         const handleChange1 = (e) => {
+            setDb(e.target.value);
+            setDb1(e.target.value);
+        };
+        
+        return (
+            <div>
+      <FormControl sx={{ m: 1, minWidth: 400 }}>
+        <InputLabel id="demo-simple-select-helper-label">Name</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          value={db}
+          label="Name"
+          onChange={handleChange}
+          >
+           {dataBase.map((el) =><MenuItem key={el.id} value={el.id}>{el.name}</MenuItem>)}
+        </Select>
+      </FormControl>
+      <FormControl sx={{ m: 1, minWidth: 300 }}>
+          <InputLabel id="demo-simple-select-helper-label">Rol</InputLabel>
+        <Select
+          value={db1}
+          onChange={handleChange1}
+          displayEmpty
+          inputProps={{ 'aria-label': 'Rol' }}
+        >
+          {dataBase.map((el) =><MenuItem key={el.id} value={el.id}>{el.rol}</MenuItem>)}
+        </Select>
+      </FormControl> 
+    </div>
   );
 }
