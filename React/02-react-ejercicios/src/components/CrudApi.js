f8457151e47382680ccfee8e7786420370baca8a
Gastondanielx4 <tochircentral@gmail.com>
2022-06-08 17:10:45 -0300

    09

---
+++ React/02-react-ejercicios/src/components/CrudApi.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { helpHttp } from "../helpers/helpHttp";
+import CrudForm from "./CrudForm";
+import CrudTable from "./CrudTable";
+
+const CrudApp = () => {
+  const [db, setDb] = useState([]);
+  const [dataToEdit, setDataToEdit] = useState(null);
+  let api = helpHttp();
+  let url = "http://localhost:5000/santos";
+
+  useEffect(() => {
+    api.get(url).then((res) => {
+      //console.log(res);
+      if (!res.err) {
+        setDb(res);
+      } else {
+        setDb(null);
+      }
+    });
+  }, []);
+
+  const createData = (data) => {
+    setDb([...db, data]);
+    data.id = Date.now();
+    console.log(data);
+  };
+
+  const updateData = (data) => {
+    let newData = db.map((el) => (el.id === data.id ? data : el));
+    setDb(newData);
+  };
+
+  const deleteData = (id) => {
+    let isDelete = window.confirm(
+      `Estas Seguro de eleminar el registro con el id ${id}`
+    );
+    if (isDelete) {
+      let newData = db.filter((el) => el.id !== id);
+      setDb(newData);
+    } else {
+      return;
+    }
+  };
+
+  return (
+    <div>
+      <h2>CRUD Api</h2>
+      <article className="grid-1-2">
+        <CrudForm
+          createData={createData}
+          updateData={updateData}
+          dataToEdit={dataToEdit}
+          setDataToEdit={setDataToEdit}
+        />
+        <CrudTable
+          data={db}
+          setDataToEdit={setDataToEdit}
+          deleteData={deleteData}
+        />
+      </article>
+    </div>
+  );
+};
+
+export default CrudApp;
