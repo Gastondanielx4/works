5a1809ff974cf673dc075d82a30c475d15ae848d
Gastondanielx4 <gastondanielx4@gmail.com>
2022-08-31 19:58:45 -0300

    31-08_19:58

---
+++ PHP/www/php-potrero/ejemplo.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ejercitamos</h1>
+     <?php
+        $mensajes= "Hola Potrero";
+        echo $mensajes
+        ?>
+    <br>
+    <hr>
+    <?php
+        $manzanas=90;
+        $naranjas= 120;
+        $total = $manzanas + $naranjas;
+        echo $total;
+    ?>
+    <br>
+    <hr>
+    <?php
+    echo $manzanas + $naranjas
+    ?>
+
+</body>
+</html>
